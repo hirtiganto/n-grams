c51b7d36e01aba71d86e6d58dedb00abeed8a55b
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-28 14:25:34 +0100

    Added code that draws lines from one mover to another

--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ function draw() {
 
   for (var i = 0; i < movers.length; i++) {
     movers[i].render()
+    movers[i].drawLine(movers)
   }
 }
 
@@ -32,10 +33,32 @@ function Mover(index, n, widht, height) {
 
   this.diameter = 25
 
+  this.skips = 0
+
+  // so this is my fancy way of determining on how many arms do i have to skip
+  // for odd number: skips = (n - 1) / 2
+  // for even numbers skips = (n - 2) / 2
+  if (n % 2 == 0) {
+    this.skips = (n - 2) / 2
+  } else {
+    this.skips = (n - 1) / 2
+  }
+
   this.render = function() {
     noStroke()
     fill(255)
     ellipse(this.x,this.y,this.diameter,this.diameter)
+  }
+
+  this.drawLine = function(others) {
+    stroke(255)
+    //line(this.x,this.y,others[this.getIndex(others)].x,others[this.getIndex(others)].y)
+    var i = this.skips + index
+    if (i >= others.length) {
+      i -= others.length
+    }
+
+    line(this.x,this.y,others[i].x, others[i].y)
   }
 
 }
